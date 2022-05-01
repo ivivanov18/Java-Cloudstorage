4a5b335186323c6043c37b7aa04e454217bfec6d
Ivan Ivanov <ivanov.ivan.v@gmail.com>
2022-05-01 13:54:31 +0300

    feature: login - add LoginController - add AuthenticationService - add initial implementation of HomeController - add th:object to login page - add configuration in SecurityConfig to call custom login page and configure authentication service

--- src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.udacity.jwdnd.course1.cloudstorage.config;
 
+import com.udacity.jwdnd.course1.cloudstorage.services.AuthenticationService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +10,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthenticationService authenticationService;
+
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationService);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -17,6 +30,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated();
 
-        http.formLogin().loginPage("/login").permitAll();
+        http.formLogin().loginPage("/login").permitAll().defaultSuccessUrl("/home");
     }
 }
---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controllers/LoginController.java
@@ -0,0 +1,26 @@
+package com.udacity.jwdnd.course1.cloudstorage.controllers;
+
+import com.udacity.jwdnd.course1.cloudstorage.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+
+    @GetMapping
+    public String getLoginPage(Model model) {
+        return "login";
+    }
+
+    @PostMapping
+    public String loginUser(@ModelAttribute("loginUser") User user, Model model) {
+
+        return "login";
+    }
+}
